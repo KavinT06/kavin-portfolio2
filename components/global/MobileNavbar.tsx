9ddb75880f8ab9faafb3f9d91c1504b011733e5d
Kavin T <itskavin47@gmail.com>
2026-02-11 22:49:10 +0530

    intial commit

---
+++ components/global/MobileNavbar.tsx
@@ -0,0 +1,163 @@
+import { useEffect, useState } from "react";
+
+import Link from "next/link";
+import {routes} from "@/data/global";
+
+export default function MobileNavbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
+    isMenuOpen,
+    {
+      enterDelay: 20,
+      exitDelay: 300,
+    }
+  );
+
+  function toggleMenu() {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+      document.body.style.overflow = "";
+    } else {
+      setIsMenuOpen(true);
+      document.body.style.overflow = "hidden";
+    }
+  }
+
+  useEffect(() => {
+    return function cleanup() {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
+  return (
+    <nav>
+      <div
+        className={`w-full justify-between flex items-center ${isMenuRendered && 'bg-bg'} p-5`}
+        style={{ zIndex: 101 }}
+      >
+        <li className="list-none font-bold text-lg">
+          <Link href="/">
+            <img
+              className="mr-3"
+              src="/static/logos/logo_full.svg"
+              width="160"
+            />
+          </Link>
+        </li>
+        <button
+          className="burger visible md:hidden"
+          aria-label="Toggle menu"
+          type="button"
+          onClick={toggleMenu}
+        >
+          <MenuIcon data-hide={isMenuOpen} />
+          <CrossIcon data-hide={!isMenuOpen} />
+        </button>
+      </div>
+      {isMenuMounted && (
+        <ul
+          className={`menu flex flex-col absolute bg-bg
+            ${isMenuRendered && "menuRendered"}`}
+        >
+          {routes.map((item, index) => {
+            return (
+              <li
+                className="border-b border-gray-900 text-gray-100 text-sm font-semibold"
+                style={{ transitionDelay: `${150 + index * 25}ms` }}
+              >
+                <Link href={item.path}>
+                  <a className="flex w-auto pb-4">{item.title}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </nav>
+  );
+}
+
+type DelayedRenderOptions = {
+  enterDelay?: number;
+  exitDelay?: number;
+};
+
+function useDelayedRender(isRendered: boolean, options: DelayedRenderOptions) {
+  const enterDelay = options?.enterDelay ?? 0;
+  const exitDelay = options?.exitDelay ?? 0;
+  const [mounted, setMounted] = useState(isRendered);
+  const [rendered, setRendered] = useState(isRendered);
+
+  useEffect(() => {
+    let mountTimeout: ReturnType<typeof setTimeout> | undefined;
+    let renderTimeout: ReturnType<typeof setTimeout> | undefined;
+
+    if (isRendered) {
+      setMounted(true);
+      renderTimeout = setTimeout(() => setRendered(true), enterDelay);
+    } else {
+      setRendered(false);
+      mountTimeout = setTimeout(() => setMounted(false), exitDelay);
+    }
+
+    return () => {
+      if (renderTimeout) {
+        clearTimeout(renderTimeout);
+      }
+      if (mountTimeout) {
+        clearTimeout(mountTimeout);
+      }
+    };
+  }, [isRendered, enterDelay, exitDelay]);
+
+  return { mounted, rendered };
+}
+
+function MenuIcon(props) {
+  return (
+    <svg
+      className="h-5 w-5 absolute text-gray-100"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M2.5 7.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.5 12.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function CrossIcon(props) {
+  return (
+    <svg
+      className="h-5 w-5 absolute text-gray-100"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="none"
+      shapeRendering="geometricPrecision"
+      {...props}
+    >
+      <path d="M18 6L6 18" />
+      <path d="M6 6l12 12" />
+    </svg>
+  );
+}
