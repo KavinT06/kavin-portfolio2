b46cf8e29b17c5d3c971bf53a7806e6857ceb504
Kavin T <itskavin47@gmail.com>
2026-04-15 08:26:53 +0530

    chore: update code structure for improved readability and maintainability

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import "tailwindcss/tailwind.css";
 import "@/styles/main.css";
 
 import { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
